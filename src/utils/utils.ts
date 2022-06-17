c1ad067c949c4cb8b7d7654ed38a9e198511ca1c
IvanPratasevich <iv.pratasevich@gmail.com>
2022-06-17 21:47:44 +0300

    fix: implement correct handling of internal error

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,7 @@
-import { IncomingMessage } from 'http';
+import { IncomingMessage, ServerResponse } from 'http';
 import { IUser } from '../interfaces/interfaces';
 
-export const getReqData = (req: IncomingMessage): Promise<IUser> => {
+export const getReqData = (req: IncomingMessage, res: ServerResponse): Promise<IUser> => {
   return new Promise((resolve, reject) => {
     try {
       let body: string = '';
@@ -9,7 +9,12 @@ export const getReqData = (req: IncomingMessage): Promise<IUser> => {
         body += chunk.toString();
       });
       req.on('end', () => {
-        resolve(JSON.parse(body));
+        try {
+          resolve(JSON.parse(body));
+        } catch (error) {
+          res.writeHead(500, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ message: 'Internal error' }));
+        }
       });
     } catch (error) {
       reject(error);
