96ebb467e5418547de67f45fcc04981fd71ff534
IvanPratasevich <iv.pratasevich@gmail.com>
2022-06-19 17:13:23 +0300

    feat: add test second scenario

--- src/server.test.ts
+++ src/server.test.ts
@@ -58,3 +58,44 @@ describe('First scenario', () => {
     expect(res.statusCode).toEqual(404);
   });
 });
+
+describe('Second scenario', () => {
+  let user: IUser;
+
+  beforeAll((done) => {
+    done();
+  });
+
+  afterAll((done) => {
+    server.close();
+    done();
+  });
+
+  it('Should answer with status code 400 and corresponding message if request body does not contain required fields', async () => {
+    const res = await request(server).post('/api/users').send({ username: 'Ivan' });
+    expect(res.statusCode).toEqual(400);
+  });
+
+  it('Should create new user', async () => {
+    const res = await request(server)
+      .post('/api/users')
+      .send({ username: 'John', age: 15, hobbies: ['dogs', 'reading', 'games'] });
+    user = res.body;
+    expect(res.statusCode).toEqual(201);
+    expect(res.body.username).toEqual('John');
+    expect(res.body.age).toEqual(15);
+    expect(res.body.hobbies).toEqual(['dogs', 'reading', 'games']);
+  });
+
+  it('Should return error if userId is invalid', async () => {
+    const invalidId = '123456789abc';
+    const res = await request(server).get(`/api/users/${invalidId}`);
+    expect(res.statusCode).toEqual(400);
+    expect(res.body.message).toEqual('User id is invalid');
+  });
+
+  it('Should delete user', async () => {
+    const res = await request(server).delete(`/api/users/${user.id}`);
+    expect(res.statusCode).toEqual(204);
+  });
+});
